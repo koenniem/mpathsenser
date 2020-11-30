b20984d829eccaca5d0e1f6ed572f2a87b215641
Koen Niemeijer <koen.niemeijer@kuleuven.be>
2020-11-30 15:20:08 +0100

    Correctly process timestamps

--- R/functions.R
+++ R/functions.R
@@ -1,5 +1,5 @@
 #' Clean up direct CARP output
-#' 
+#'
 #' When copying data directly coming from CARP, JSON files are often corrupt due to the
 #' app not properly closing them. This function (hopefully) fixes that. Additionally,
 #' it can also unzip all the output in the directory.
@@ -11,16 +11,16 @@
 #' @export
 clean <- function(path = getwd(), zipped = TRUE) {
 	# 1. Get list of JSONs
-	
+
 	# Find all JSON files that are _not_ zipped
 	# Thus, make sure you didn't unzip them yet, otherwise this may take a long time
 	files <- dir(path = path, pattern = "*.json$")
-	
+
 	# 2. Fix JSONs
 	if(length(files > 0)) {
 		lines <- lapply(files, readLines)
 		eof <- lapply(lines, function(x) x[length(x)])
-		
+
 		for(i in 1:length(files)) {
 			file <- paste0(path, "/", files[i])
 			if(eof[i] == ",") {
@@ -36,7 +36,7 @@ clean <- function(path = getwd(), zipped = TRUE) {
 	} else{
 		message("No JSON files found. Checking if there are zips...")
 	}
-	
+
 	# 3. Unzip
 	zipfiles <- dir(pattern = "*.zip$", full.names = TRUE)
 	if(length(zipfiles) > 0) {
@@ -45,7 +45,7 @@ clean <- function(path = getwd(), zipped = TRUE) {
 	} else {
 		message("No files found to unzip.")
 	}
-	
+
 	if(zipfiles == 0 && files == 0) {
 		warning("No files were found to be processed.")
 	}
@@ -61,14 +61,14 @@ clean <- function(path = getwd(), zipped = TRUE) {
 #' @return An S3 object containing a tibble for each sensor in the data.
 #' @export
 import <- function(path = getwd(), progress = TRUE, parallel = TRUE) {
-	
+
 	# Retrieve all JSON files
 	files <- dir(path = path, pattern = "*.json$")
-	
+
 	if(length(files) == 0) {
 		stop("No JSON files found")
 	}
-	
+
 	# Set up parallel backend
 	if(parallel) {
 		doFuture::registerDoFuture()
@@ -76,65 +76,78 @@ import <- function(path = getwd(), progress = TRUE, parallel = TRUE) {
 	} else {
 		foreach::registerDoSEQ()
 	}
-	
+
 	if(progress) {
 		progressr::with_progress({
-			res <- CARP:::import_impl(path, files)
+			res <- import_impl(path, files)
 		})
 	} else{
-		res <- CARP:::import_impl(path, files)
+		res <- import_impl(path, files)
 	}
-	
+
 	# Return to sequential processing
 	if(parallel) {
 		future::plan(future::sequential)
 	}
-	
+
 	# TODO: # Replace with non-purrr version
-	res <- purrr::compact(res)
-	res <- purrr::flatten(res) 
+	res <- purrr::flatten(res)
 	res <- tibble::enframe(res)
+
+	# Split by sensor
 	res <- split(res, res$name)
+
 	res <- lapply(res, function(x) tidyr::unnest(x, value))
-	# res <- lapply(res, function(x) x[["timestamp"]] <- )
+
+	# Only keep elements that have rows (i.e. not 0)
+	res <- res[sapply(res, function(x) nrow(x) != 0)]
+
+	# ISO8601 to R data format
+	for(i in 1:length(res)) {
+		if("timestamp" %in% colnames(res[[i]])) {
+			res[[i]][["timestamp"]] <- lubridate::as_datetime(res[[i]][["timestamp"]])
+			res[[i]][["timestamp"]] <- lubridate::force_tz(res[[i]][["timestamp"]], "Europe/Brussels")
+		}
+	}
+
 	res
-}	
+}
 
 import_impl <- function(path, files) {
 	p <- progressr::progressor(length(files))
-	res <- foreach::`%dopar%`(foreach::foreach(i = 1:length(files), .packages = "CARP"), {
-		
+	res <- foreach::`%dopar%`(foreach::foreach(i = 1:length(files)), {
+
 		# Update progress bar
 		p(sprintf("x=%g", i))
-	
+
 		data <- rjson::fromJSON(file = paste0(path, "/", files[i]), simplify = FALSE)
-		
+
 		# Check if it is not an empty file
 		if(length(data) == 0) {
 			# next
 			return(NULL)
 		}
-		
+
 		# Clean-up and extract the header and body
 		data <- tibble::tibble(header = lapply(data, function(x) x[1]),
 													 body = lapply(data, function(x) x[2]))
 		data$header <- lapply(data$header, as.data.frame)
 		data <- tidyr::unnest(data, header)
 		colnames(data) <- c("study_id", "user_id", "start_time", "data_namespace", "sensor", "body")
-		
+
 		# Convert to POSIX time
 		data$start_time <- as.POSIXct(data$start_time, "%Y-%m-%dT%H:%M:%S", tz="Europe/Brussels")
-		
+
 		# Divide et impera
 		data <- split(data, as.factor(data$sensor), drop = TRUE)
-		
+
 		# Drop useless data
 		data[["error"]] <- NULL
 		data[["unknown"]] <- NULL
-		
+
 		out <- vector("list", length(data))
 		names(out) <- names(data)
-		
+
 		# Call function for each list according to their name plus _fun
 		for(i in 1:length(data)) {
 			# Get sensor name
@@ -143,7 +156,7 @@ import_impl <- function(path, files) {
 
 			# Make the package for the decision function explicition
 			# otherwise globals won't know to export it
-			out[[i]] <- CARP:::which_sensor(tmp, sensor)
+			out[[i]] <- which_sensor(tmp, sensor)
 		}
 		out
 	})
@@ -156,4 +169,7 @@ acceleration <- function(data, x = x, y = y, z = z) {
 	data$acceleration <- sqrt((x)^2 + (y)^2 + (z - 9.810467)^2)
 }
 
+coverage <- function(data) {
+
+}
 
--- R/sensor_functions.R
+++ R/sensor_functions.R
@@ -4,29 +4,29 @@
 # 	} else {
 # 		assign(name, data, envir = .GlobalEnv)
 # 	}
-# } 
+# }
 
 which_sensor <- function(data, sensor) {
 	switch(sensor,
-				 accelerometer = CARP:::accelerometer_fun(data),
-				 activity = CARP:::activity_fun(data),
-				 air_quality = CARP:::air_quality_fun(data),
-				 app_usage = CARP:::app_usage_fun(data),
-				 apps = CARP:::apps_fun(data),
-				 battery = CARP:::battery_fun(data),
-				 bluetooth = CARP:::bluetooth_fun(data),
-				 calendar = CARP:::calendar_fun(data),
-				 connectivity = CARP:::connectivity_fun(data),
-				 device = CARP:::device_fun(data),
-				 gyroscope = CARP:::gyroscope_fun(data),
-				 light = CARP:::light_fun(data),
-				 location = CARP:::location_fun(data),
-				 memory = CARP:::memory_fun(data),
-				 mobility = CARP:::mobility_fun(data),
-				 screen = CARP:::screen_fun(data),
-				 text_message = CARP:::text_message_fun(data),
-				 weather = CARP:::weather_fun(data),
-				 wifi = CARP:::wifi_fun(data))
+				 accelerometer = accelerometer_fun(data),
+				 activity = activity_fun(data),
+				 air_quality = air_quality_fun(data),
+				 app_usage = app_usage_fun(data),
+				 apps = apps_fun(data),
+				 battery = battery_fun(data),
+				 bluetooth = bluetooth_fun(data),
+				 calendar = calendar_fun(data),
+				 connectivity = connectivity_fun(data),
+				 device = device_fun(data),
+				 gyroscope = gyroscope_fun(data),
+				 light = light_fun(data),
+				 location = location_fun(data),
+				 memory = memory_fun(data),
+				 mobility = mobility_fun(data),
+				 screen = screen_fun(data),
+				 text_message = text_message_fun(data),
+				 weather = weather_fun(data),
+				 wifi = wifi_fun(data))
 }
 
 default_fun <- function(data) {
@@ -35,9 +35,9 @@ default_fun <- function(data) {
 	data <- tidyr::unnest(data, body)
 	# data$timestamp <- as.POSIXct(data$timestamp, "%Y-%m-%dT%H:%M:%S", tz="Europe/Brussels")
 	data$id <- NULL
-	
-	if(nrow(data) > 0) { 
-		return(data)	
+
+	if(nrow(data) > 0) {
+		return(data)
 	} else {
 		return(NA)
 	}
@@ -52,20 +52,20 @@ accelerometer_fun <- function(data) {
 }
 
 activity_fun <- function(data) {
-	CARP:::default_fun(data)
+	default_fun(data)
 }
 
 air_quality_fun <- function(data) {
-	CARP:::default_fun(data)
+	default_fun(data)
 }
 
 app_usage_fun <- function(data) {
-	CARP:::default_fun(data)
+	default_fun(data)
 }
 
 apps_fun <- function(data) {
 	data$body <- lapply(data$body, function(x) x$body)
-	data$body <- lapply(data$body, function(x) tibble::tibble(timestamp = x$timestamp, apps = list(x$installed_apps)))
+	data$body <- lapply(data$body, function(x) tibble::tibble(apps = list(x$installed_apps)))
 	data <- tidyr::unnest(data, body)
 	# data$timestamp <- as.POSIXct(data$timestamp, "%Y-%m-%dT%H:%M:%S", tz="Europe/Brussels")
 	data
@@ -73,7 +73,7 @@ apps_fun <- function(data) {
 
 # TODO: incorrect app usage
 battery_fun <- function(data) {
-	CARP:::default_fun(data)
+	default_fun(data)
 }
 
 bluetooth_fun <- function(data) {
@@ -88,8 +88,8 @@ calendar_fun <- function(data) {
 	data$body <- lapply(data$body, function(x) x$body$calendar_events)
 	data$body <- lapply(data$body, function(x) lapply(x, function(y) unlist(y, recursive = FALSE)))
 	data$body <- lapply(data$body, dplyr::bind_rows)
-	data <- tidyr::unnest(data, body)
-	
+	data <- tidyr::unnest(data, body, keep_empty = TRUE)
+
 	if(nrow(data) > 0) {
 		data$start <- as.POSIXct(data$start, "%Y-%m-%dT%H:%M:%S", tz="Europe/Brussels")
 		data$end <- as.POSIXct(data$end, "%Y-%m-%dT%H:%M:%S", tz="Europe/Brussels")
@@ -98,47 +98,47 @@ calendar_fun <- function(data) {
 }
 
 connectivity_fun <- function(data) {
-	CARP:::default_fun(data)
+	default_fun(data)
 }
 
 device_fun <- function(data) {
-	CARP:::default_fun(data)
+	default_fun(data)
 }
 
 gyroscope_fun <- function(data) {
-	CARP:::accelerometer_fun(data)
+	accelerometer_fun(data)
 }
 
 light_fun <- function(data) {
-	CARP:::default_fun(data)
+	default_fun(data)
 }
 
 location_fun <- function(data) {
-	CARP:::default_fun(data)
+	default_fun(data)
 }
 
 memory_fun <- function(data) {
-	CARP:::default_fun(data)
+	default_fun(data)
 }
 
 # TODO: find out how this works
 mobility_fun <- function(data) {
-	CARP:::default_fun(data)
+	default_fun(data)
 }
 
 screen_fun <- function(data) {
-	CARP:::default_fun(data)
+	default_fun(data)
 }
 
 # TODO: Check if text_message can be unnested
 text_message_fun <- function(data) {
-	CARP:::default_fun(data)
+	default_fun(data)
 }
 
 weather_fun <- function(data) {
-	CARP:::default_fun(data)
+	default_fun(data)
 }
 
 wifi_fun <- function(data) {
-	CARP:::default_fun(data)
+	default_fun(data)
 }
