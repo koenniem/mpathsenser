4d53d6d065dbde708c3859082d6e6b33a68021bd
Koen Niemeijer <koen.niemeijer@kuleuven.be>
2022-03-04 18:24:24 +0100

    Reverse geocoding

--- R/location_functions.R
+++ R/location_functions.R
@@ -6,19 +6,21 @@
 #' @return The non-lazy decrypted GPS data
 #' @export
 decrypt_gps <- function(data, key) {
+	if (!is.raw(key) & !is.character(key)) stop("key must be either a character or raw vector")
+
 	if (!is.raw(key)) {
 		key <- sodium::hex2bin(key)
 	}
 
 	data %>%
 		dplyr::collect() %>%
-		dplyr::mutate(
-			dplyr::across(c(latitude, longitude), ~ {
-				lapply(.x, sodium::hex2bin) %>%
-					lapply(sodium::simple_decrypt, key) %>%
-					lapply(rawToChar) %>%
-					unlist
-			}))
+		dplyr::mutate(dplyr::across(c(latitude, longitude), ~ {
+			.x %>%
+				lapply(sodium::hex2bin) %>%
+				lapply(sodium::simple_decrypt, key) %>%
+				lapply(rawToChar) %>%
+				unlist
+		}))
 }
 
 deg2rad <- function(deg) {
@@ -37,7 +39,7 @@ rad2deg <- function(rad) {
 #' @param lat1 The latitude of point 1 in degrees.
 #' @param lon2 The longitude of point 2 in degrees.
 #' @param lat2 The latitude of point 2 in degrees.
-#' @param r The average earth radius (6371 km by default).
+#' @param r The average earth radius.
 #'
 #' @return The distance between point 1 and 2 in kilometers.
 #' @export
@@ -55,7 +57,7 @@ haversine <- function(lat1, lon1, lat2, lon2, r = 6371) {
 	dlon <- lon2 - lon1
 	dlat <- lat2 - lat1
 	a <- sin(dlat / 2)^2 + cos(lat1) * cos(lat2) * sin(dlon / 2)^2
-	c <- 2 * atan2(sqrt(a), sqrt(1 - a))
+	c <- 2 * asin(sqrt(a))
 	d <- r * c
 	d
 }
@@ -69,4 +71,41 @@ location_variance <- function(lat, lon, time) {
 	return(log(lat_sd^2 + lon^2))
 }
 
+#' Reverse geocoding with latitude and longitude
+#'
+#' This functions allows you to extract information about a place based on the latitude and
+#' longitude from the OpenStreetMaps nominatim API.
+#'
+#' Warning: DO NOT ABUSE THIS FUNCTION or you will be banned by OpenStreetMap. The maximum number
+#' of requests is around 1 per second. Also make sure not to do batch lookups, as many subsequent
+#' requests will get you blocked as well.
+#'
+#' @param lat The latitude of the location (in degrees)
+#' @param lon The longitude of the location (in degrees)
+#' @param zoom The desired zoom level from 1-18. The lowest level, 18, is building level.
+#' @param email If you are making large numbers of request please include an appropriate email
+#' address to identify your requests. See Nominatim's Usage Policy for more details.
+#'
+#' @return A list of information about the location. See [Nominatim's documentation](https://nominatim.org/release-docs/develop/api/Reverse/#example-with-formatjsonv2) for more details.
+#' @export
+#'
+#' @examples
+#' # Frankfurt Airport
+#' geocode_rev(50.037936, 8.5599631)
+geocode_rev <- function(lat, lon, zoom = 18, email=NULL) {
+	base_query <- "https://nominatim.openstreetmap.org/reverse.php?"
+	args <- list(
+		lat = lat,
+		lon = lon,
+		email = rep(email, length(lat)),
+		zoom = rep(zoom, length(lat)),
+		format = rep("jsonv2", length(lat))
+	)
 
+	args <- purrr::flatten(args)
+	args <- purrr::transpose(args)
+	args <- lapply(args, function(x) paste0(names(x) , "=", x, collapse = "&"))
+	query <- lapply(args, function(x) paste0(base_query, x))
+	res <- lapply(query, jsonlite::fromJSON, query)
+	jsonlite::fromJSON(paste0(base_query, args))
+}
