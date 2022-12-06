a8161652aa3b35ed82b1bced0b41b32488eebe7b
Koen Niemeijer <koen.niemeijer@kuleuven.be>
2022-12-06 17:10:35 +0100

    Updated indexes, case insensitive

--- inst/extdata/indexes.sql
+++ inst/extdata/indexes.sql
@@ -1,149 +1,149 @@
-CREATE INDEX idx_accelerometer ON Accelerometer(participant_id);
+CREATE INDEX idx_accelerometer ON accelerometer(participant_id);
 
-CREATE INDEX idx_airquality ON AirQuality(participant_id);
+CREATE INDEX idx_airquality ON airquality(participant_id);
 
-CREATE INDEX idx_activity ON Activity(participant_id);
+CREATE INDEX idx_activity ON activity(participant_id);
 
-CREATE INDEX idx_appusage ON AppUsage(participant_id);
+CREATE INDEX idx_appusage ON appusage(participant_id);
 
-CREATE INDEX idx_battery ON Battery(participant_id);
+CREATE INDEX idx_battery ON battery(participant_id);
 
-CREATE INDEX idx_bluetooth ON Bluetooth(participant_id);
+CREATE INDEX idx_bluetooth ON bluetooth(participant_id);
 
-CREATE INDEX idx_calendar ON Calendar(participant_id);
+CREATE INDEX idx_calendar ON calendar(participant_id);
 
-CREATE INDEX idx_connectivity ON Connectivity(participant_id);
+CREATE INDEX idx_connectivity ON connectivity(participant_id);
 
-CREATE INDEX idx_device ON Device(participant_id);
+CREATE INDEX idx_device ON device(participant_id);
 
-CREATE INDEX idx_error ON Error(participant_id);
+CREATE INDEX idx_error ON error(participant_id);
 
-CREATE INDEX idx_geofence ON Geofence(participant_id);
+CREATE INDEX idx_geofence ON geofence(participant_id);
 
-CREATE INDEX idx_gyroscope ON Gyroscope(participant_id);
+CREATE INDEX idx_gyroscope ON gyroscope(participant_id);
 
-CREATE INDEX idx_installedapps ON InstalledApps(participant_id);
+CREATE INDEX idx_installedapps ON installedapps(participant_id);
 
-CREATE INDEX idx_keyboard ON Keyboard(participant_id);
+CREATE INDEX idx_keyboard ON keyboard(participant_id);
 
-CREATE INDEX idx_light ON Light(participant_id);
+CREATE INDEX idx_light ON light(participant_id);
 
-CREATE INDEX idx_location ON Location(participant_id);
+CREATE INDEX idx_location ON location(participant_id);
 
-CREATE INDEX idx_memory ON Memory(participant_id);
+CREATE INDEX idx_memory ON memory(participant_id);
 
-CREATE INDEX idx_mobility ON Mobility(participant_id);
+CREATE INDEX idx_mobility ON mobility(participant_id);
 
-CREATE INDEX idx_noise ON Noise(participant_id);
+CREATE INDEX idx_noise ON noise(participant_id);
 
-CREATE INDEX idx_pedometer ON Pedometer(participant_id);
+CREATE INDEX idx_pedometer ON pedometer(participant_id);
 
-CREATE INDEX idx_phonelog ON PhoneLog(participant_id);
+CREATE INDEX idx_phonelog ON phonelog(participant_id);
 
-CREATE INDEX idx_screen ON Screen(participant_id);
+CREATE INDEX idx_screen ON screen(participant_id);
 
-CREATE INDEX idx_textmessage ON TextMessage(participant_id);
+CREATE INDEX idx_textmessage ON textmessage(participant_id);
 
-CREATE INDEX idx_weather ON Weather(participant_id);
+CREATE INDEX idx_weather ON weather(participant_id);
 
-CREATE INDEX idx_wifi ON Wifi(participant_id);
+CREATE INDEX idx_wifi ON wifi(participant_id);
 
-CREATE INDEX date_accelerometer ON Accelerometer(date)
+CREATE INDEX date_accelerometer ON accelerometer(date)
 
-CREATE INDEX date_airquality ON AirQuality(date)
+CREATE INDEX date_airquality ON airquality(date)
 
-CREATE INDEX date_activity ON Activity(date)
+CREATE INDEX date_activity ON activity(date)
 
-CREATE INDEX date_appusage ON AppUsage(date)
+CREATE INDEX date_appusage ON appusage(date)
 
-CREATE INDEX date_battery ON Battery(date)
+CREATE INDEX date_battery ON battery(date)
 
-CREATE INDEX date_bluetooth ON Bluetooth(date)
+CREATE INDEX date_bluetooth ON bluetooth(date)
 
-CREATE INDEX date_calendar ON Calendar(date)
+CREATE INDEX date_calendar ON calendar(date)
 
-CREATE INDEX date_connectivity ON Connectivity(date)
+CREATE INDEX date_connectivity ON connectivity(date)
 
-CREATE INDEX date_device ON Device(date)
+CREATE INDEX date_device ON device(date)
 
-CREATE INDEX date_error ON Error(date)
+CREATE INDEX date_error ON error(date)
 
-CREATE INDEX date_geofence ON Geofence(date)
+CREATE INDEX date_geofence ON geofence(date)
 
-CREATE INDEX date_gyroscope ON Gyroscope(date)
+CREATE INDEX date_gyroscope ON gyroscope(date)
 
-CREATE INDEX date_installedapps ON InstalledApps(date)
+CREATE INDEX date_installedapps ON installedapps(date)
 
-CREATE INDEX date_keyboard ON Keyboard(date)
+CREATE INDEX date_keyboard ON keyboard(date)
 
-CREATE INDEX date_light ON Light(date)
+CREATE INDEX date_light ON light(date)
 
-CREATE INDEX date_location ON Location(date)
+CREATE INDEX date_location ON location(date)
 
-CREATE INDEX date_memory ON Memory(date)
+CREATE INDEX date_memory ON memory(date)
 
-CREATE INDEX date_mobility ON Mobility(date)
+CREATE INDEX date_mobility ON mobility(date)
 
-CREATE INDEX date_noise ON Noise(date)
+CREATE INDEX date_noise ON noise(date)
 
-CREATE INDEX date_pedometer ON Pedometer(date)
+CREATE INDEX date_pedometer ON pedometer(date)
 
-CREATE INDEX date_phonelog ON PhoneLog(date)
+CREATE INDEX date_phonelog ON phonelog(date)
 
-CREATE INDEX date_screen ON Screen(date)
+CREATE INDEX date_screen ON screen(date)
 
-CREATE INDEX date_textmessage ON TextMessage(date)
+CREATE INDEX date_textmessage ON textmessage(date)
 
-CREATE INDEX date_weather ON Weather(date)
+CREATE INDEX date_weather ON weather(date)
 
-CREATE INDEX date_wifi ON Wifi(date)
+CREATE INDEX date_wifi ON wifi(date)
 
-CREATE INDEX date_idx_accelerometer ON Accelerometer(participant_id, date)
+CREATE INDEX date_idx_accelerometer ON accelerometer(participant_id, date)
 
-CREATE INDEX date_idx_airquality ON AirQuality(participant_id, date)
+CREATE INDEX date_idx_airquality ON airquality(participant_id, date)
 
-CREATE INDEX date_idx_activity ON Activity(participant_id, date)
+CREATE INDEX date_idx_activity ON activity(participant_id, date)
 
-CREATE INDEX date_idx_appusage ON AppUsage(participant_id, date)
+CREATE INDEX date_idx_appusage ON appusage(participant_id, date)
 
-CREATE INDEX date_idx_battery ON Battery(participant_id, date)
+CREATE INDEX date_idx_battery ON battery(participant_id, date)
 
-CREATE INDEX date_idx_bluetooth ON Bluetooth(participant_id, date)
+CREATE INDEX date_idx_bluetooth ON bluetooth(participant_id, date)
 
-CREATE INDEX date_idx_calendar ON Calendar(participant_id, date)
+CREATE INDEX date_idx_calendar ON calendar(participant_id, date)
 
-CREATE INDEX date_idx_connectivity ON Connectivity(participant_id, date)
+CREATE INDEX date_idx_connectivity ON connectivity(participant_id, date)
 
-CREATE INDEX date_idx_device ON Device(participant_id, date)
+CREATE INDEX date_idx_device ON device(participant_id, date)
 
-CREATE INDEX date_idx_error ON Error(participant_id, date)
+CREATE INDEX date_idx_error ON error(participant_id, date)
 
-CREATE INDEX date_idx_geofence ON Geofence(participant_id, date)
+CREATE INDEX date_idx_geofence ON geofence(participant_id, date)
 
-CREATE INDEX date_idx_gyroscope ON Gyroscope(participant_id, date)
+CREATE INDEX date_idx_gyroscope ON gyroscope(participant_id, date)
 
-CREATE INDEX date_idx_installedapps ON InstalledApps(participant_id, date)
+CREATE INDEX date_idx_installedapps ON installedapps(participant_id, date)
 
-CREATE INDEX date_idx_keyboard ON Keyboard(participant_id, date)
+CREATE INDEX date_idx_keyboard ON keyboard(participant_id, date)
 
-CREATE INDEX date_idx_light ON Light(participant_id, date)
+CREATE INDEX date_idx_light ON light(participant_id, date)
 
-CREATE INDEX date_idx_location ON Location(participant_id, date)
+CREATE INDEX date_idx_location ON location(participant_id, date)
 
-CREATE INDEX date_idx_memory ON Memory(participant_id, date)
+CREATE INDEX date_idx_memory ON memory(participant_id, date)
 
-CREATE INDEX date_idx_mobility ON Mobility(participant_id, date)
+CREATE INDEX date_idx_mobility ON mobility(participant_id, date)
 
-CREATE INDEX date_idx_noise ON Noise(participant_id, date)
+CREATE INDEX date_idx_noise ON noise(participant_id, date)
 
-CREATE INDEX date_idx_pedometer ON Pedometer(participant_id, date)
+CREATE INDEX date_idx_pedometer ON pedometer(participant_id, date)
 
-CREATE INDEX date_idx_phonelog ON PhoneLog(participant_id, date)
+CREATE INDEX date_idx_phonelog ON phonelog(participant_id, date)
 
-CREATE INDEX date_idx_screen ON Screen(participant_id, date)
+CREATE INDEX date_idx_screen ON screen(participant_id, date)
 
-CREATE INDEX date_idx_textmessage ON TextMessage(participant_id, date)
+CREATE INDEX date_idx_textmessage ON textmessage(participant_id, date)
 
-CREATE INDEX date_idx_weather ON Weather(participant_id, date)
+CREATE INDEX date_idx_weather ON weather(participant_id, date)
 
-CREATE INDEX date_idx_wifi ON Wifi(participant_id, date)
+CREATE INDEX date_idx_wifi ON wifi(participant_id, date)
